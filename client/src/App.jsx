bfc36d6dc88e634899c1e901137aa6e1fa111815
Michel Ribbens <michelribbens@gmail.com>
2021-04-10 11:54:57 +0100

    testing signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link, Redirect, Switch } from 'react-router-dom';
+import SignUp from './views/SignUp';
 // import Navbar from './components/Navbar';
 
-function App() {
-	return (
-		<div className='App'>
-			{/* <Navbar /> */}
-			Test test test
-		</div>
-	);
+export class App extends Component {
+	render() {
+		return (
+			<div>
+				<BrowserRouter>
+					<Switch>
+						{/* <Route path="/" component={Home} exact /> */}
+						<Route path='/sign-up' component={SignUp} redirect='/' exact />
+					</Switch>
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
+
 export default App;
