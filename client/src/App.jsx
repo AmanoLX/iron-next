b0022586bd0943823e923a994147d69c58f13170
Sok Mun Wunder <sokmunwunder@gmail.com>
2021-04-10 16:52:28 +0200

    Home Page Testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link, Redirect, Switch } from 'react-router-dom';
 import SignUp from './views/SignUp';
+import Home from './views/Home';
 // import Navbar from './components/Navbar';
 
 export class App extends Component {
-	render() {
-		return (
-			<div>
-				Hi welcome
-				<BrowserRouter>
-					<Switch>
-						{/* <Route path="/" component={Home} exact /> */}
-						<Route path='/sign-up' component={SignUp} redirect='/' exact />
-					</Switch>
-				</BrowserRouter>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        Hi welcome
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/sign-up" component={SignUp} redirect="/" exact />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
